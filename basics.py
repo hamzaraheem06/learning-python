fb8a4eed628957548bb5863c9b633493a740dfcf
Hamza Raheem <hamzaraheem06@gmail.com>
2024-11-23 23:26:36 +0500

    learned about basic string methods

--- basics.py
+++ basics.py
@@ -1,4 +1,27 @@
-print("Hello World")
+# print("Hello World")
 
-for i in range(1, 10, 3):
-    print("Even" if i%2 == 0 else "Odd")
+
+# for i in range(1, 10, 3):
+#     print("Even" if i%2 == 0 else "Odd")
+#
+# formatted string
+# Hamza, like in javascript you can use `` to inject variables in a string, in Python you can do that by using f with the starting quotation of the string to tell the compiler this is a special type of string that you want to format in your way.
+
+# name = input("What is your name? ").strip().title() input is a function that allow us to take an input value from the user. It returns whatever user types in a str format.
+#
+# nameSize = len(name)
+
+# print(f"Hello, {name}") # print is a function that allow us to write some value on the terminal
+# print(name.count('a', 0, len(name))) for counting the number of times a substring occurs in a string
+# print(name.endswith('b')) # to check if a string ends with a specific character(s), you can also use this for a specific location in the string with start and end parameters.
+# print("H\tA\tM\tZ\tA".expandtabs(tabsize=8)) Hamza you are intelligent you can understand this
+
+# name = input("What is your name? ").strip()
+# print(name.isalpha())
+
+name = "Hamza Raheem"
+names = [first, last] = name.split(" ")
+print(names[1])
+
+# print(name.rfind('a', 0, len(name))) finds looks for a specific character(s) in a string, returns the index of the first instance, else returns -1 if there is no instance of the character(s) in the string
+
