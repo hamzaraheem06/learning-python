2bca5b8e4daa3b1318af23b33b431783de6e4576
Hamza Raheem <hamzaraheem06@gmail.com>
2024-11-24 22:27:04 +0500

    learned about strings in python

--- basics.py
+++ basics.py
@@ -1,5 +1,5 @@
 # print("Hello World")
-
+from pyexpat.errors import messages
 
 # for i in range(1, 10, 3):
 #     print("Even" if i%2 == 0 else "Odd")
@@ -19,9 +19,45 @@
 # name = input("What is your name? ").strip()
 # print(name.isalpha())
 
-name = "Hamza Raheem"
-names = [first, last] = name.split(" ")
-print(names[1])
+# name = "Hamza Raheem"
+# names = [first, last] = name.split(" ")
+# print(names[1])
 
 # print(name.rfind('a', 0, len(name))) finds looks for a specific character(s) in a string, returns the index of the first instance, else returns -1 if there is no instance of the character(s) in the string
+
+# message = "Hello, Hamza!"
+# print(message)
+
+# **********Python Crash Course*************
+# Exercise 2.1 and 2.2
+# message = "Hello, Hamza"
+# print(message)
+#
+# message = "Hello, Ayesha"
+# print(message)
+
+# title = "how to fail at almost everything, and still win big".title()
+#
+# auther = "Micheal Jackson"
+#
+# titleAuther = title.join(auther)
+#
+# print(titleAuther)
+
+# Exercise 2.3 - 2.7
+# name = "Ayesha eman"
+# message = "Would you like to go out on a date?"
+
+# print(f"Hello {name.title()}, {message}
+
+# print(name.lower())
+# print(name.upper())
+# print(name.title())
+
+# famousPerson = "Ghost"
+# famousQuote = "\"Choices are consequences\""
+# print(f"{famousPerson} once said, {famousQuote}")
+
+# name = " \n \t       Vyrat\nKholi      "
+# print(name.rstrip())
 
